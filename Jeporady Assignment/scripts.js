b82040ebb691a24bb43cd5e526c5080f2da60c6c
Leo Gothberg <leo.gothberg@gmail.com>
2017-03-18 13:57:33 -0700

    more things

--- Jeporady Assignment/scripts.js
+++ Jeporady Assignment/scripts.js
@@ -1,5 +1,38 @@
-function closeModal() {
-	$("#closeModal").onclick = function() {
-		modal.style.display = "none";
-	}
+function getQuestions() {
+	var modal = $(this).parent().attr('id');
+	alert('modal');
 }
+
+// function displayModal() {
+// 	modal.style.display = "block";
+// }
+
+// function showAnswer() {
+// 	modal.find(".question").style.display = "none;"
+// }
+
+// // Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+// 	modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+// 	modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+// 	if (event.target == modal) {
+// 		modal.style.display = "none";
+// 	}
+// }
